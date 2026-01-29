1d0d16190276356b94bbf4a527d0a0b6d3f2501a
MalekToumi-815 <191920560+MalekToumi-815@users.noreply.github.com>
2026-01-29 15:43:51 +0100

    Add project files.

---
+++ Dental App/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Dental_App.Views;
+using Prism.DryIoc;
+
+namespace Dental_App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {   
+        protected override Window CreateShell()
+        {
+            return ContainerLocator.Container.Resolve<MainView>();
+        }
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register any types needed for dependency injection here
+        }
+    }
+
+}
---
+++ Dental App/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace Dental_App.ViewModels
+{
+    public class MainViewModel : BindableBase
+    {
+        private String title = "hello bibi"; 
+        public String Title
+        {
+            get { return title; }
+            set { SetProperty(ref title, value); }
+        }
+    }
+}
